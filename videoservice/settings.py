17d2ba143e4e873ea57b2c15b99cf3107ae68e32
Gautam singh <gautamk1512>
2023-04-06 01:03:17 +0530

    handl cord

--- videoservice/settings.py
+++ videoservice/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = '0e44#p=0jk-&f530#1gb2cn@s0mbes1&)=1e7jn%s6jwo9=&-9'
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CORS_ALLOWED_ORIGINS = ["*"]
 
 
 # Application definition
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'ckeditor'
+    'ckeditor',
+    'cors-headers '
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'videoservice.urls'
